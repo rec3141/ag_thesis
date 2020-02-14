b270c1ad3a29e8a9c7df6b59ce1e76b7de6f9673
R. Eric Collins <rec3141@gmail.com>
2020-02-14 16:59:50 -0600

    Add files via upload

--- merge_patric_indices.R
+++ merge_patric_indices.R
@@ -9,6 +9,8 @@ library(DECIPHER)
 library(phytools)
 library(ape)
 library(rlang)
+library(dendextend)
+library(colorspace)
 
 # first run:
 # 1) growthrates_intro.r to calculate growth rates
@@ -117,22 +119,22 @@ colsave = readRDS(paste0(genus,"_save.RDS"))
 
 #### plot heatmaps
 
-mat = as.matrix(colsave[[1]])
+# load local families
+mat = as.matrix(colsave[[2]])
 mat[is.infinite(mat)] = NA
 mat[is.nan(mat)] = NA
 mat[mat==0] = NA
 
-mattmp = mat[rowSums(!is.na(mat)) > 0.9*ncol(mat), ]
-#remove genomes that don't have most core proteins
-mattmp = mattmp[,colSums(!is.na(mattmp)) > 0.9*nrow(mattmp)]
-matcols = colnames(mattmp)
-matrows = rownames(mattmp)
+# remove protein families that aren't in most genomes
+mattmp = mat[rowSums(!is.na(mat)) > 0.8*ncol(mat), ]
+# remove genomes that don't have most core proteins
+mattmp = mattmp[,colSums(!is.na(mattmp)) > 0.8*nrow(mattmp)]
 
 # set up phylogeny
 
 # keep only genomes that are in both the tree and the amino acid indices
-mattmp = mattmp[,intersect(tre.phylo$tip.label,colnames(mattmp))]
-dend = keep.tip(tre.phylo,intersect(tre.phylo$tip.label,colnames(mattmp)))
+mattmp = mattmp[,intersect(tre.phylo$tip.label, colnames(mattmp))]
+dend = keep.tip(tre.phylo, intersect(tre.phylo$tip.label,colnames(mattmp)))
 
 # clean up and reorder tree
 dend$node.label = NULL # Need to remove node labels
@@ -143,7 +145,7 @@ dend = drop.tip(dend,"1380381.3") #drop long tip (poor genome assembly)
 # convert from 'phylo' to 'dendrogram'
 dend1 = ReadDendrogram(textConnection(write.tree(dend))) 
 dend1 = as.dendrogram(dend1)
-dend.order = order.dendrogram(dend1)
+dend1 = set(dend1, "branches_lwd", 5)
 
 #reorder matrix to match dendrogram
 mattmp = mattmp[,labels(dend1)]
@@ -220,10 +222,16 @@ coldiff = matrix(nrow=ncol(colfams),ncol=ncol(colfams))
 colnames(coldiff) = colnames(colfams)
 rownames(coldiff) = colnames(colfams)
 
-for(i in 1:length(colpick)) {
-  for(j in 1:length(colpick)) {
-      dfs = rownames(colfams)[ setdiff( which(colfams[,colpick[i]]), which(colfams[,colpick[j]])) ]
-      if(!is_empty(dfs)) coldiff[i,j] = length(dfs)
+colsim = coldiff
+coluniq = coldiff
+
+for(j in 1:nrow(coldiff)) {
+  for(k in 1:nrow(coldiff)) {
+      dfs = rownames(colfams)[ setdiff( which(colfams[,j]), which(colfams[,k])) ]
+      sim = rownames(colfams)[ intersect( which(colfams[,j]), which(colfams[,k])) ]
+      # unq = rownames(colfams)[ intersect( which(colfams[,j]), which(colfams[,k])) & !]
+      if(!is_empty(dfs)) coldiff[j,k] = length(dfs)
+      if(!is_empty(sim)) colsim[j,k] = length(sim)
   }
 }
 
@@ -232,6 +240,12 @@ coldiffs = coldiff + t(coldiff)
 keepcol = intersect(labels(dend1),colnames(coldiffs))
 
 coldiffs = coldiffs[keepcol,keepcol]
+colsim = colsim[keepcol, keepcol]
+
+coldiffs = coldiffs * lower.tri(coldiffs)
+colsim = colsim * lower.tri(colsim)
+
+coldiffs = coldiffs + t(colsim)
 
 ### distances
 dist.tre = cophenetic.phylo(tre.phylo)
@@ -244,19 +258,131 @@ rownames(coldiffs) = genomes$genome.genome_name[match(rownames(coldiffs),genomes
 colnames(coldiffs) = rownames(coldiffs)
 
 # make plots
-pdf(file=paste0("coldiffs_",protfam,"_pctdiff.pdf"))
-plot(dist.plot,coldiffs,pch=19,col=rgb(0,0,1,0.3), xlab="Phylogenetic Distance", ylab="Different Protein Families", main=protfam)
-plot(dist.plot,100*coldiffs/sizemat,pch=19,col=rgb(0,0,1,0.3), xlab="Phylogenetic Distance", ylab="Percent Different Protein Families", main=protfam)
+pdf(file=paste0("coldiffs_",protfam,"_pctdiff.pdf"),width=12,height=6)
+par(mfrow=c(1,2))
+
+# shared fams
+x = dist.plot*upper.tri(dist.plot)
+x[x==0] = NA
+y = coldiffs*upper.tri(coldiffs)
+y[y==0] = NA
+plot(x,y,pch=19,col=rgb(0,0,1,0.3), xlab="Phylogenetic Distance", ylab="Shared Protein Families", main=protfam)
+
+y = 100*(coldiffs*upper.tri(coldiffs))/(sizemat*upper.tri(sizemat))
+plot(x,y,pch=19,col=rgb(0,0,1,0.3), xlab="Phylogenetic Distance", ylab="Percent Shared Protein Families", main=protfam)
+
+# non-shared fams
+x = dist.plot*lower.tri(dist.plot)
+x[x==0] = NA
+y = coldiffs*lower.tri(coldiffs)
+y[y==0] = NA
+plot(x,y,pch=19,col=rgb(0,0,1,0.3), xlab="Phylogenetic Distance", ylab="Unshared Protein Families", main=protfam)
+
+y = 100*(coldiffs*lower.tri(coldiffs))/(sizemat*lower.tri(sizemat))
+plot(x,y,pch=19,col=rgb(0,0,1,0.3), xlab="Phylogenetic Distance", ylab="Percent Unshared Protein Families", main=protfam)
+
 dev.off()
 
+# heatmap of shared/unshared
 pdf(file=paste0("coldiffs_",protfam,".pdf"),width=48,height=48)
-heatmap.2(coldiffs,scale="none",trace="none", RowSideColors = rowcols, ColSideColors = rowcols, Rowv = dend1, Colv = dend1, cellnote = coldiffs, notecol="black",margins=c(14,14), notecex = 0.8)
+heatmap.2(coldiffs,scale="none",trace="none", RowSideColors = rowcols, ColSideColors = rowcols, 
+          Rowv = dend1, Colv = dend1, cellnote = coldiffs, notecol="white", col=diverge_hcl(25),
+          margins=c(55,55), notecex = 1.1, cexRow = 2.5, cexCol = 2.5, key=TRUE, denscol="black", key.par=list(cex=4))
 dev.off()
 
 #colpgf = unique(strsplit(x = paste(coldiff,collapse=" "),split=" ")[[1]])
 #write.table(colpgf,file="tmp.pgf",row.names = FALSE,col.names = FALSE,quote=F)
 
 }
+
+
+##### linear models
+
+plfdat = read.table(paste0(genus,"_families_local.tsv"),sep="\t",row.names = 1, stringsAsFactors=F, quote="",comment="")[,3,drop=F]
+pgfdat = read.table(paste0(genus,"_families_global.tsv"),sep="\t",row.names=1, stringsAsFactors=F, quote="",comment="")[,3,drop=F]
+famdat = rbind(plfdat,pgfdat)
+colnames(famdat) = "description"
+
+# dend.clust = dendextend::cutree(dend1,4)
+# dend.clust = dend.clust+1
+dend.clust=1:length(labels(dend1))
+names(dend.clust) = labels(dend1)
+labels(dend1)
+dend1 = set(dend1, "branches_lwd", 3)
+
+pdf(file="volcano.pdf", width=10,height=6)  
+
+for(j in 1:length(colsave)) {
+  print(j)
+  mat = as.matrix(colsave[[j]])
+  mat[is.infinite(mat)] = NA
+  mat[is.nan(mat)] = NA
+  
+  common_ids = intersect(colnames(mat),ratk$id.patric)
+  
+  ratmat = mat[,common_ids]
+
+  ogt = ratk[match(colnames(ratmat),ratk$id.patric),"topt"]
+
+  ratmat = ratmat[rowSums(!is.na(ratmat)) > 0,] #8023
+  ratmat = ratmat[rowSums(!is.na(ratmat)) > 1,] #6593
+  ratmat = ratmat[rowSums(!is.na(ratmat)) > 2,] #4196
+  ratmat = ratmat[rowSums(!is.na(ratmat)) > 3,] #4011
+
+  cormat = apply(ratmat,1,function(x) (cor.test(x,ogt)[c("estimate","p.value")]))
+  corout = data.frame(matrix(unlist(cormat),ncol=2,byrow=T))
+  rownames(corout) = names(cormat)
+  colnames(corout) = c("r","p.value")
+  
+  corout = corout[which(abs(corout$r)<1),] #remove perfect matches (artifacts)
+  
+  pmin = 0.1/nrow(corout)
+
+  plot(corout$r,log10(corout$p.value),pch=19,col=rgb(0,0,1,0.3), main=names(colsave)[j])
+  lines(x=c(-1,1),y=c(log10(pmin),log10(pmin)))
+
+  corout = corout[order(corout$r,corout$p.value,decreasing=c(TRUE,FALSE)),]
+  getgenes = rownames(corout)[corout$p.value < pmin]
+  if(length(getgenes)<1) next
+  
+  genenames = famdat[getgenes,"description"]
+  
+  colpick = dend.clust[colnames(ratmat)]
+  names(colpick) = colnames(ratmat)
+  cols = rainbow_hcl(length(dend.clust),l=50,c=100)[colpick]
+  
+    for(i in 1:length(getgenes)) {
+    k = getgenes[i]
+    
+    cols[is.na(cols)] = "#888888"
+    
+    par(mfcol=c(1,2),mar = c(4,4,4,4))
+
+    plot(ogt,ratmat[k,],
+         xlab = "Optimal Growth Temperature", ylab=names(colsave)[j], 
+         main=(paste0(names(colsave)[j],"\n",k,"\n",genenames[i],"\n","r = ",round(corout[k,"r"],3),", p = 10^",round(log10(corout[k,"p.value"]),3))), 
+         cex.main=0.7, pch=19, col=cols)
+    abline(lm(ratmat[k,]~ogt))
+    
+    plot(dend1,horiz=T,xaxt="n",yaxt="n",leaflab="none")
+    dend.cols = rainbow_hcl(length(dend.clust),l=50,c=100)
+    dend.cols[!(dend.clust %in% colpick[!is.na(ratmat[k,])])] = NA
+    dend.cols = cbind(dend.cols, rowcols)
+    colored_bars(dend.cols,horiz=T,rowLabels = NA, add=T)
+    
+  }
+  
+  }
+dev.off()
+
+
+
+# to work with PATRIC from R
+# requires PATRIC installation
+# may have to change location of PATRIC environment executable
+# patricapp='/Applications/PATRIC.app//user-env.sh'
+# write.table(file="tmp.pat",getgenes,quote=F,col.names = F,row.names = F)
+# genenames = system(paste0("source ",patricapp," > /dev/null && cat tmp.pat | p3-get-family-data --nohead | cut -f4"),intern=TRUE)
 
 
 ##pangenome/local
@@ -302,66 +428,6 @@ polygon(c(1:ng, rev(1:ng)), c(apply(perm.pangenome,1,min), rev(apply(perm.pangen
 points(1:ng,mean.pangenome,type='l')
 points(1:ng,mean.core,type='l')
 
-dev.off()
-
-
-##### linear models
-
-# requires PATRIC installation
-# may have to change location of PATRIC environment executable
-
-# to work with PATRIC from R
-# patricapp='/Applications/PATRIC.app//user-env.sh'
-# write.table(file="tmp.pat",getgenes,quote=F,col.names = F,row.names = F)
-# genenames = system(paste0("source ",patricapp," > /dev/null && cat tmp.pat | p3-get-family-data --nohead | cut -f4"),intern=TRUE)
-
-plfdat = read.table(paste0(genus,"_families_local.tsv"),sep="\t",row.names = 1, stringsAsFactors=F, quote="",comment="")[,3,drop=F]
-pgfdat = read.table(paste0(genus,"_families_global.tsv"),sep="\t",row.names=1, stringsAsFactors=F, quote="",comment="")[,3,drop=F]
-famdat = rbind(plfdat,pgfdat)
-colnames(famdat) = "description"
-
-pdf(file="volcano.pdf")  
-
-for(j in 1:length(colsave)) {
-  print(j)
-  mat = as.matrix(colsave[[j]])
-  mat[is.infinite(mat)] = NA
-  mat[is.nan(mat)] = NA
-  
-  ratmat = mat[,colnames(mat) %in% ratk$id.patric]
-
-  ogt = ratk$topt[match(colnames(ratmat),ratk$id.patric)]
-
-  ratmat = ratmat[rowSums(!is.na(ratmat)) > 0,] #8023
-  ratmat = ratmat[rowSums(!is.na(ratmat)) > 1,] #6593
-  ratmat = ratmat[rowSums(!is.na(ratmat)) > 2,] #4196
-  ratmat = ratmat[rowSums(!is.na(ratmat)) > 3,] #4011
-
-  cormat = apply(ratmat,1,function(x) (cor.test(x,ogt)[c("estimate","p.value")]))
-  corout = data.frame(matrix(unlist(cormat),ncol=2,byrow=T))
-  rownames(corout) = names(cormat)
-  colnames(corout) = c("r","p.value")
-  
-  corout = corout[which(abs(corout$r)<1),] #remove perfect matches (artifacts)
-  
-  pmin = 0.05/nrow(corout)
-
-  plot(corout$r,log10(corout$p.value),pch=19,col=rgb(0,0,1,0.3), main=names(colsave)[j])
-  lines(x=c(-1,1),y=c(log10(pmin),log10(pmin)))
-
-  corout = corout[order(corout$r,corout$p.value,decreasing=c(TRUE,FALSE)),]
-  getgenes = rownames(corout)[which(corout$p.value<pmin)]
-  if(length(getgenes)<1) next
-  
-  genenames = famdat[getgenes,"description"]
-  
-    for(i in 1:length(getgenes)) {
-    k = getgenes[i]
-    plot(ogt,ratmat[k,],xlab = "Optimal Growth Temperature", ylab=names(colsave)[j], main=paste0(names(colsave)[j],"\n",k,"\n",genenames[i]), cex.main=0.7)
-    abline(lm(ratmat[k,]~ogt))
-  }
-  
-  }
 dev.off()
 
 
